11f8f3755c1336fb558cafd82ab7fefc343f29e5
Joel Singh <joelsingh788@gmail.com>
2024-05-25 18:18:45 -0400

    Initial commit

---
+++ config.lua
@@ -0,0 +1,10 @@
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Telescope",
+  c = {"<cmd>Telescope command_history<cr>", "Command History"},
+  b = {"<cmd>Telescope buffers<cr>", "Buffers"},
+}
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
