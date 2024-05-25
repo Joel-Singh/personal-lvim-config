3134fdeaf68eb754835cd1e3b141f0995aebe964
Joel Singh <joelsingh788@gmail.com>
2024-05-25 18:47:20 -0400

    Add keymaps for switching buffers

--- config.lua
+++ config.lua
@@ -4,6 +4,9 @@ lvim.builtin.which_key.mappings["t"] = {
   b = {"<cmd>Telescope buffers<cr>", "Buffers"},
   o = {"<cmd>Telescope oldfiles<cr>", "Old Files"},
 }
+lvim.keys.normal_mode["<S-h>"] = "<cmd>bprevious<cr>"
+lvim.keys.normal_mode["<S-l>"] = "<cmd>bnext<cr>"
+lvim.keys.normal_mode["<S-q>"] = "<cmd>BufferKill<cr>"
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
