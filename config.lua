8dda58fa30814fd4c4172977b9523a86526cd5ab
Joel Singh <joelsingh788@gmail.com>
2024-11-03 06:50:00 -0500

    Format

--- config.lua
+++ config.lua
@@ -1,3 +1,9 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
 lvim.builtin.which_key.mappings["t"] = {
   name = "Telescope",
   c = {"<cmd>Telescope command_history<cr>", "Command History"},
@@ -44,8 +50,3 @@ function RunCurrentFile()
   end
 end
 
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Example configs: https://github.com/LunarVim/starter.lvim
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
